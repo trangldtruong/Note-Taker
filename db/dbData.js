e5375b99c9343e3b9a470bc267455c20a4f31192
Trang Truong <trangldtruong@gmail.com>
2023-11-15 23:04:16 -0500

    db files completed

---
+++ db/dbData.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const util = require('util');
+const uuidv1 = require('uuid');
+
+const readDislay = util.promisify(fs.readFile);
+const writeDisplay = util.promisify(fs.writeFile);
+
+class Store {
+    read(){
+        return readDislay('db/db.json', 'utf8')
+    } 
+    write(note) {
+        return writeDisplay('db/db.json', JSON.stringify(note))
+    }
+    getNotes(){
+        return this.read().then((notes) => {
+            let displayNotes;
+            try {
+                displayNotes = [].concat(JSON.parse(notes))
+            } catch (error) {
+                displayNotes = [];
+            }
+            return displayNotes
+        })
+    }
+    addNote(note){
+        const {title,text} = note;
+        if (!title || !text) {
+            throw new Error('Please enter a title and a text')
+            
+        }
+        const newNote = {title, text, id:uuid()}
+        return this.getNotes()
+        .then((notes) => [...notes, newNote])
+        .then((updatedNotes) => this.write(updatedNotes))
+        .then(() => newNote)
+
+    }
+}
+module.exports = new Store();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,19 @@
 const router = require('express').Router;
+const store = require('../db/dbData');
+
 
 router.get('/notes', (req, res) => {
+    store
+    .getNotes()
+    .then((notes) => {
+        return res.json(notes)
+    })
+    .catch((err) => res.status(500).json(err))
 
 })
 
 router.post('/notes', (req, res) => {
 
 })
+
+module.exports = router();
